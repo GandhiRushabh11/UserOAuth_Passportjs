b984bd55b83078342e78b881e5839a1dd2498522
GandhiRushabh11 <gandhirushabh11@gmail.com>
2023-12-08 22:08:43 +0530

    Connecting with DB

--- app.js
+++ app.js
@@ -1,13 +1,17 @@
 const express = require("express");
 const UserRoute = require("./routes/auth");
+const ConnectDB = require("./config/db");
 const dotenv = require("dotenv");
 dotenv.config({ path: "./config/config.env" });
 const app = express();
 app.set("view engine", "ejs");
 const PassportSetup = require("./config/passport-setup");
+
+//Connecting With DB
+ConnectDB();
+
 //Mouting User Route
 app.use("/auth", UserRoute);
-app;
 
 app.get("/", (req, res) => {
   res.render("home");
